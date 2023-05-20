8f2d9df5a7e359f7eda6c460b39005fc99c843a9
PrezBala <>
2023-05-20 19:48:58 +0000

    add background color to .app in app.css and also shortened app-header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
+        </header>
         <div className="layout">
             <div>Movie list</div>
             <div>Movie details</div> 
         </div>
-      </header>
     </div>
   );
 }
